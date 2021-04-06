bcbe3691c7c5346da43310808d21128b1e6e596f
LeeByeongHoon <fjdksl1632@naver.com>
2021-04-06 20:08:17 +0900

    create one spare test

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,19 @@
 public class Game {
+	private int[] rolls = new int[21];
+	private int currentRoll = 0;
 
 	private int score =0;
 
-	public void roll(int pins) {
 
+	public void roll(int pins) {
 		score +=pins;
+		rolls[currentRoll ++] = pins;
 	}
 
 	public int score() {
-		return 0;
+		int score=0;
+		for(int i=0;i<rolls.length; i++) {
+			score+=rolls[i];
+		}
 		return score;
 	}
-
-}
