dcbd245164fc1d41391a6c48810dec3d7a629a7f
LeeByeongHoon <fjdksl1632@naver.com>
2021-04-06 19:32:39 +0900

    create rollMany method

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -1,5 +1,5 @@
+@@ -1,30 +1,33 @@
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -7,21 +7,28 @@ import org.junit.Test;
 public class BowlingGameTest {
 
 	private Game g;
+	protected Game g;
+
 	@Before
 	public void setUp(){
+	public void setUp() {
 		g = new Game();
+	}
+
+	private void rollMany(int n, int pins) {
+		for(int i=0;i<n;i++) {
+			g.roll(pins);
+		}
 	}
 
 	@Test
 	public void testGutterGame() {
-		Game g = new Game();
-		for(int i=0;i<20;i++) {
 		for (int i = 0; i < 20; i++)
 			g.roll(0);
-		}
-		assertEquals(0,g.score());
 
 		assertEquals(0, g.score()); 
+		rollMany(20, 0);
+		assertEquals(0,g.score());
 	}
 
 	@Test
@@ -30,5 +37,8 @@ public class BowlingGameTest {
 			g.roll(1);
 
 		assertEquals(20, g.score());
+	public void testAllOnes() {
+		rollMany(20, 1);
+		assertEquals(20,g.score());
 	}
 }
